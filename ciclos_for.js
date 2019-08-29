7acf48e1d9b31b66e8af8599ad55b08879eb8315
andres <andreescampos@hotmail.com>
2019-08-29 16:31:31 -0600

    For of

--- ciclos_for.js
+++ ciclos_for.js
@@ -1,4 +1,22 @@
+//For	in	accediendo	al	valor	de	una	variable	dentro	de	un	objeto:
 var list = { a: 1, b: 2, c: 3 };
 for (var i in list) {
     console.log(list[i]); //	1,	2,	3
+}
+// For	in	accediendo	al	índice	de	una	variable	dentro	de	un	objeto;
+var list2 = { a: 1, b: 2, c: 3 };
+for (var i in list2) {
+    console.log(i); //	a,	b,	c
+}
+// For	of	accediendo	al	valor	de	una	variable	dentro	de	un	array:
+var list3 = ["a", "b", "c"];
+for (var _i = 0, list3_1 = list3; _i < list3_1.length; _i++) {
+    var b = list3_1[_i];
+    console.log(b); //	a,	b,	c
+}
+//For	of	accediendo	al	valor	de	una	variable	dentro	de	un	string:
+var string = "soy yo a quien estas buscando?";
+for (var _a = 0, string_1 = string; _a < string_1.length; _a++) {
+    var char = string_1[_a];
+    console.log(char);
 }
--- ciclos_for.ts
+++ ciclos_for.ts
@@ -1,4 +1,29 @@
-let	list	=	{a:	1,	b:	2,	c:3};
-for(let	i	in	list){
-		console.log(list[i]);	//	1,	2,	3
+//For	in	accediendo	al	valor	de	una	variable	dentro	de	un	objeto:
+
+let list = { a: 1, b: 2, c: 3 };
+for (let i in list) {
+  console.log(list[i]); //	1,	2,	3
 }
+
+// For	in	accediendo	al	índice	de	una	variable	dentro	de	un	objeto;
+
+let list2 = { a: 1, b: 2, c: 3 };
+for (let i in list2) {
+  console.log(i); //	a,	b,	c
+}
+
+// For	of	accediendo	al	valor	de	una	variable	dentro	de	un	array:
+
+let list3 = ["a", "b", "c"];
+for (let b of list3) {
+  console.log(b); //	a,	b,	c
+}
+
+/*
+//For	of	accediendo	al	valor	de	una	variable	dentro	de	un	string:
+let string = "soy yo a quien estas buscando?";
+for (let char of string) {
+  console.log(char);
+}
+*/
+
