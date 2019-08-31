bd2ab78b25c8af68e2a0355af51684c2030f44e2
andres <andreescampos@hotmail.com>
2019-08-31 14:40:17 -0600

    Desestructuracion

---
+++ desestructuracion.ts
@@ -0,0 +1,18 @@
+//Desestructuración	de objetos
+var	obj	=	{x:	1,	y:	2,	z:	3};
+console.log(obj.x);	//	1
+
+var	{x,	y,	z}	=	obj;
+console.log(x);	//	1
+
+//Desestructuración	de arrays
+var	array	=	[1,	2,	3];
+console.log(array[0]);	//	1
+
+var	[x,	y,	z]	=	array;
+console.log(x);	//	1
+
+//Desestructuración	de arrays con estructuración
+var	array	=	[1,	2,	3,	4];
+var	[x,	y,	...rest]	=	array;
+console.log(x,	y,	rest);	//	1,	2,	[3,4]
